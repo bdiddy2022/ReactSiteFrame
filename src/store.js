10021306eb6049c87fbdd552ffdb760e29c414b1
bdiddy2022 <brendan.t.downs@gmail.com>
2023-03-11 16:30:00 -0500

    Created persistent local storage with Redux

--- src/store.js
+++ src/store.js
@@ -1,10 +1,23 @@
 import { legacy_createStore, combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import { todos } from "./todos/reducers";
 
 const reducers = {
     todos,
 };
 
-const rootReducer = combineReducers(reducers);
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2,
+};
 
-export const configureStore = () => legacy_createStore(rootReducer);
+
+
+
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => legacy_createStore(persistedReducer);
