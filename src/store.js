889b77faf6587084b86e1c23b6edd85f28086e00
bdiddy2022 <brendan.t.downs@gmail.com>
2023-03-15 17:32:43 -0400

    added thunk to redux, no functionality

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
-import { legacy_createStore, combineReducers } from "redux";
+import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import { todos } from "./todos/reducers";
 
 const reducers = {
