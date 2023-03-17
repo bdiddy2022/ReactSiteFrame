0fe4cb948b757fdf9361952a864297b09052f1bb
bdiddy2022 <brendan.t.downs@gmail.com>
2023-03-16 20:32:14 -0400

    added logic for observing loading, loadSuccess, and loadFailure events. also integrated server into project

---
+++ client/src/todos/TodoList.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import TodoListItem from './TodoListItem';
+import NewTodoForm from './NewTodoForm';
+import { markTodoAsCompleted, removeTodo } from './actions';
+import './TodoList.css';
+import { loadTodos } from './thunks';
+
+
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
+    useEffect(() => {
+        startLoadingTodos();
+    }, []);
+    
+    const loadingMessage = (<div>Loading Todos...</div>);
+    const content = (
+        <div className='list-wrapper'>
+            <NewTodoForm />
+            {todos.map(todo => <TodoListItem
+                todo={todo}
+                onRemovePressed={onRemovePressed}
+                onCompletedPressed={onCompletedPressed}
+            />
+            )}
+        </div>
+    );
+
+    return (isLoading ? loadingMessage : content);
+};
+
+const mapStateToProps = state => ({
+    isLoading: state.isLoading,
+    todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch => ({
+    startLoadingTodos: () => dispatch(loadTodos()),
+    onRemovePressed: text => dispatch(removeTodo(text)),
+    onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- client/src/todos/reducers.js
+++ client/src/todos/reducers.js
@@ -1,7 +1,33 @@
-import { CREATE_TODO, MARK_TODO_AS_COMPLETED, REMOVE_TODO } from './actions'
+import {
+    CREATE_TODO,
+    MARK_TODO_AS_COMPLETED,
+    REMOVE_TODO,
+    LOAD_TODOS_IN_PROGRESS,
+    LOAD_TODOS_SUCCESS,
+    LOAD_TODOS_FAILURE,
+} from './actions';
 
 //Reducer: function named after whatever resource its in charge of managing
 //Reducers take the current state as well as a triggered action, then updates the state based on the result of the triggered action
+
+export const isLoading = (state = false, action) => {
+    const { type } = action;
+
+    switch (type) {
+        case LOAD_TODOS_IN_PROGRESS: {
+            return true;
+        }
+        case LOAD_TODOS_SUCCESS: {
+
+        }
+        case LOAD_TODOS_FAILURE: {
+            return false;
+        }
+        default: {
+            return state;
+        }
+    }
+};
 
 export const todos = (state = [], action) => {
     const { type, payload } = action;
@@ -23,7 +49,7 @@ export const todos = (state = [], action) => {
         case MARK_TODO_AS_COMPLETED: {
             const { text } = payload;
             return state.map(todo => {
-                if(todo.text===text){
+                if (todo.text === text) {
                     return { ...todo, isCompleted: true };
                 }
                 return todo;
